1ce8d25e0d962b6668c377ef3cfdeca8188c36a8
chunkychunk2 <49397604+chunkychunk2@users.noreply.github.com>
2021-10-07 13:07:31 +0300

    Create test.java

---
+++ test.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int i;
+        if (a > b) i = a;
+        else  i = b;
+            while (true) {
+                if (b % i == 0 && a % i == 0) break;
+                else i = i - 1;
+            }
+            System.out.println(i);
+    }
+}
